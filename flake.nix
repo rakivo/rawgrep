531f93d41f25c3c6ae13c6e042c8bbdc931515e8
Mark Tyrkba <awfulmarco@gmail.com>
2025-12-18 23:30:58 +0300

    chore: flake.nix tweaks

--- flake.nix
+++ flake.nix
@@ -11,66 +11,58 @@
     crane,
     ...
   }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = system: nixpkgs.legacyPackages.${system};
+
     mkRawgrep = system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = pkgsFor system;
       craneLib = crane.mkLib pkgs;
       src = craneLib.cleanCargoSource ./.;
-      
       commonArgs = {
         inherit src;
         strictDeps = true;
-        nativeBuildInputs = [ pkgs.capnproto ];
+        nativeBuildInputs = [pkgs.capnproto];
         buildInputs = [];
       };
-      
       cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-    in craneLib.buildPackage (commonArgs // {
-      inherit cargoArtifacts;
-      meta = with pkgs.lib; {
-        description = "The fastest grep in the world";
-        homepage = "https://github.com/rakivo/rawgrep";
-        license = licenses.mit;
-        maintainers = [];
+    in
+      craneLib.buildPackage (commonArgs
+        // {
+          inherit cargoArtifacts;
+          meta = with pkgs.lib; {
+            description = "The fastest grep in the world";
+            homepage = "https://github.com/rakivo/rawgrep";
+            license = licenses.mit;
+            maintainers = [];
+          };
+        });
+  in {
+    packages = forAllSystems (system: {
+      default = mkRawgrep system;
+    });
+
+    apps = forAllSystems (system: {
+      default = {
+        type = "app";
+        program = "${mkRawgrep system}/bin/rawgrep";
+        meta.description = "Run rawgrep";
       };
     });
-  in {
-    packages.x86_64-linux.default = mkRawgrep "x86_64-linux";
-    packages.aarch64-linux.default = mkRawgrep "aarch64-linux";
 
-    apps.x86_64-linux.default = {
-      type = "app";
-      program = "${mkRawgrep "x86_64-linux"}/bin/rawgrep";
-      meta.description = "Run rawgrep";
-    };
-    apps.aarch64-linux.default = {
-      type = "app";
-      program = "${mkRawgrep "aarch64-linux"}/bin/rawgrep";
-      meta.description = "Run rawgrep";
-    };
-
-    devShells.x86_64-linux.default = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in pkgs.mkShell {
-      inputsFrom = [ (mkRawgrep "x86_64-linux") ];
-      buildInputs = with pkgs; [
-        rustc
-        cargo
-        rust-analyzer
-        rustfmt
-        clippy
-      ];
-    };
-    devShells.aarch64-linux.default = let
-      pkgs = nixpkgs.legacyPackages.aarch64-linux;
-    in pkgs.mkShell {
-      inputsFrom = [ (mkRawgrep "aarch64-linux") ];
-      buildInputs = with pkgs; [
-        rustc
-        cargo
-        rust-analyzer
-        rustfmt
-        clippy
-      ];
-    };
+    devShells = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in {
+      default = pkgs.mkShell {
+        inputsFrom = [(mkRawgrep system)];
+        buildInputs = with pkgs; [
+          rustc
+          cargo
+          rust-analyzer
+          rustfmt
+          clippy
+        ];
+      };
+    });
   };
 }
