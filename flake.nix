3edb975670793cf6e0bd01799944b68a047e9a55
Mark Tyrkba <awfulmarco@gmail.com>
2025-12-18 23:15:43 +0300

    fix: stop using flake-utils

--- flake.nix
+++ flake.nix
@@ -8,51 +8,51 @@
 
   outputs = {
     nixpkgs,
-      crane,
-      flake-utils,
-      ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+    crane,
+    ...
+  }: let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = system: nixpkgs.legacyPackages.${system};
+
+    mkRawgrep = system: let
+      pkgs = pkgsFor system;
       craneLib = crane.mkLib pkgs;
-
       src = craneLib.cleanCargoSource ./.;
-
       commonArgs = {
         inherit src;
         strictDeps = true;
-
-        nativeBuildInputs = [
-          pkgs.capnproto
-        ];
-
+        nativeBuildInputs = [ pkgs.capnproto ];
         buildInputs = [];
       };
-
       cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+    in craneLib.buildPackage (commonArgs // {
+      inherit cargoArtifacts;
+      meta = with pkgs.lib; {
+        description = "The fastest grep in the world";
+        homepage = "https://github.com/rakivo/rawgrep";
+        license = licenses.mit;
+        maintainers = [];
+      };
+    });
+  in {
+    packages = forAllSystems (system: {
+      default = mkRawgrep system;
+    });
 
-      rawgrep = craneLib.buildPackage (commonArgs // {
-        inherit cargoArtifacts;
-
-        meta = with pkgs.lib; {
-          description = "The fastest grep in the world";
-          homepage = "https://github.com/rakivo/rawgrep";
-          license = licenses.mit;
-          maintainers = [];
-        };
-      });
-    in {
-      packages.default = rawgrep;
-
-      apps.default = {
+    apps = forAllSystems (system: {
+      default = {
         type = "app";
-        program = "${rawgrep}/bin/rawgrep";
+        program = "${mkRawgrep system}/bin/rawgrep";
         meta.description = "Run rawgrep";
       };
+    });
 
-      devShells.default = pkgs.mkShell {
-        inputsFrom = [rawgrep];
-
+    devShells = forAllSystems (system: let
+      pkgs = pkgsFor system;
+    in {
+      default = pkgs.mkShell {
+        inputsFrom = [ (mkRawgrep system) ];
         buildInputs = with pkgs; [
           rustc
           cargo
@@ -62,4 +62,5 @@
         ];
       };
     });
+  };
 }
