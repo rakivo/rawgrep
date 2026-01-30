ae8b7a7bfa612f7b543776e6bfdc6ef791d3dc65
Mark Tyrkba <awfulmarco@gmail.com>
2026-01-30 17:53:33 +0300

    feat: some minor convenience stuff for emacs integration

--- emacs/rawgrep.el
+++ emacs/rawgrep.el
@@ -1,11 +1,33 @@
 ;;; rawgrep.el --- Emacs integration for rawgrep -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2025
+
 ;; Author: Mark Tyrkba <marktyrkba456@gmail.com>
-;; Version: 0.1.1
+;; URL: https://github.com/rakivo/rawgrep
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: tools, grep, search
-;; URL: https://github.com/rakivo/rawgrep
+
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
 ;;; Commentary:
 ;; This package provides Emacs integration for rawgrep, a fast grep alternative.
 ;; It allows you to interactively search using rawgrep with jumpable results.
@@ -20,6 +42,7 @@
 ;;   (rawgrep-set-keybinding)
 
 ;;; Code:
+
 (defgroup rawgrep nil
   "Emacs integration for rawgrep."
   :group 'tools
@@ -48,17 +71,19 @@
 (defvar rawgrep-args-map
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map minibuffer-local-map)
-    (define-key map (kbd "C-d") 'rawgrep-switch-to-path)
+    (define-key map (kbd "C-;") 'rawgrep-switch-to-path)
     map)
   "Keymap for rawgrep arguments input.
-Use C-d to switch to path editing.")
+Use C-; to switch to path editing.")
 
 (defvar rawgrep-path-map
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map minibuffer-local-filename-completion-map)
+    (define-key map (kbd "C-;") 'rawgrep-switch-to-args)
     (define-key map (kbd "C-g") 'rawgrep-cancel-path)
     map)
   "Keymap for rawgrep path input.
+Use C-; to switch back to arguments editing.
 Use C-g to cancel and return to arguments editing.")
 
 (defun rawgrep-switch-to-path ()
@@ -82,31 +107,35 @@ Use C-g to cancel and return to arguments editing.")
 (defun rawgrep ()
   "Perform rawgrep search with interactive arguments and path editing.
 Enter your search pattern and any flags (e.g., 'pattern --case-sensitive -i').
-Press RET to search in current directory.
-Press C-d to switch to path editing.
+Press RET to execute search.
+Press C-; to switch to path editing.
 While editing path:
+  - Press RET or C-; to return to arguments editing
   - Press C-g to cancel path changes and return to arguments
-  - Press RET to execute search
 History navigation works as usual: M-p/M-n, C-p/C-n, or up/down arrows.
 Press C-g while editing arguments to cancel the search entirely."
   (interactive)
   (unless rawgrep-executable
     (error "Cannot find rawgrep executable.  Please set `rawgrep-executable'"))
-  
+
   (let ((args (or rawgrep--last-args ""))
         (path default-directory)
         (done nil))
-    
+
     (condition-case nil
         (progn
           ;; Arguments editing loop
           (while (not done)
             (setq rawgrep--switch-to-path nil)
-            
+
             (let ((minibuffer-local-map rawgrep-args-map))
-              (setq args (read-string (format "Args (pattern and flags) (C-d for dir-path) [path: %s]: " path)
-                                      args)))
-            
+              (minibuffer-with-setup-hook
+                  (lambda ()
+                    ;; Move cursor to the beginning of input
+                    (goto-char (minibuffer-prompt-end)))
+                (setq args (read-string (format "Args (pattern and flags) (C-; for path) [path: %s]: " path)
+                                        args))))
+
             (if rawgrep--switch-to-path
                 ;; User wants to edit path
                 (condition-case nil
@@ -115,25 +144,25 @@ Press C-g while editing arguments to cancel the search entirely."
                         (setq rawgrep--switch-to-path nil
                               rawgrep--switch-to-args nil
                               rawgrep--cancel-path nil)
-                        
+
                         (let ((minibuffer-local-map rawgrep-path-map))
-                          (setq path (read-directory-name 
-                                      (format "Path (C-g to cancel) [args: %s]: " args)
+                          (setq path (read-directory-name
+                                      (format "Path (RET or C-; to return to args, C-g to cancel) [args: %s]: " args)
                                       path)))
-                        
-                        (unless rawgrep--switch-to-args
-                          ;; User pressed RET on path, we're done
-                          (setq done t))))
+
+                        ;; After path editing, always return to args editing
+                        ;; (whether user pressed RET or C-;)
+                        (setq rawgrep--switch-to-path nil)))
                   (quit
                    ;; User pressed C-g, cancel path changes and go back to args
                    (setq rawgrep--cancel-path nil)))
               ;; User pressed RET on args, we're done
               (setq done t)))
-          
+
           ;; Save for next time
           (setq rawgrep--last-args args
                 rawgrep--last-path path)
-          
+
           ;; Execute the search
           (let ((command (format "%s %s --jump %s"
                                  rawgrep-executable
@@ -152,4 +181,5 @@ Users may prefer to set this manually in their init.el."
   (message "Rawgrep keybinding set: M-e"))
 
 (provide 'rawgrep)
+
 ;;; rawgrep.el ends here
